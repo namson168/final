0606e0083fd3dbb2950717bd40a9a0702d2a3c93
sollabeck <kevin.ceballos@gmail.com>
2015-05-29 18:50:14 -0700

    try to change tab spacing

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,26 +1,26 @@
 class ChargesController < ApplicationController
 
-	def new
-	end
+  def new
+  end
 
-	def create
-		date = Playdate.find(params[:date_id])
-		@amount = (date.total_cost_per_person * 100) # amount in cents
+  def create
+    date = Playdate.find(params[:date_id])
+    @amount = (date.total_cost_per_person * 100) # amount in cents
 
-		customer = Stripe::Customer.create(
-			email:  params[:stripeEmail],
-			card:   params[:stripeToken]
+    customer = Stripe::Customer.create(
+      email:  params[:stripeEmail],
+      card:   params[:stripeToken]
       )
 
-		charge = Stripe::Charge.create(
+    charge = Stripe::Charge.create(
       customer:    customer.id,
       amount:      @amount,
       description: date.id,
       currency:    'usd'
       )
 
-		if current_user == date.initiator.parent
-			date.initiator_paid = true
+    if current_user == date.initiator.parent
+      date.initiator_paid = true
       date.save!
     elsif current_user == date.recipient.parent
      date.recipient_paid = true
