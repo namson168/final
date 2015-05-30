133fe5374eea1ac740b2c75d090d557324b01958
sollabeck <kevin.ceballos@gmail.com>
2015-05-29 18:48:36 -0700

    clean up code

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,6 @@
 class ChargesController < ApplicationController
 
 	def new
-
 	end
 
 	def create
@@ -11,48 +10,48 @@ class ChargesController < ApplicationController
 		customer = Stripe::Customer.create(
 			email:  params[:stripeEmail],
 			card:   params[:stripeToken]
-		)
+      )
 
 		charge = Stripe::Charge.create(
-		  customer:    customer.id,
-		  amount:      @amount,
-		  description: date.id,
-		  currency:    'usd'
-		)
+      customer:    customer.id,
+      amount:      @amount,
+      description: date.id,
+      currency:    'usd'
+      )
 
 		if current_user == date.initiator.parent
 			date.initiator_paid = true
       date.save!
-		elsif current_user == date.recipient.parent
-			date.recipient_paid = true
-      date.save!
-    end
+    elsif current_user == date.recipient.parent
+     date.recipient_paid = true
+     date.save!
+   end
 
-		if date.status == 'paid'
-			notify(date)
-		end
+   if date.status == 'paid'
+     notify(date)
+   end
 
-		redirect_to "/users/#{current_user.id}?message=paid&date=#{date.id}"
+   redirect_to "/users/#{current_user.id}?message=paid&date=#{date.id}"
 
-		rescue Stripe::CardError, Stripe::InvalidRequestError => e
-		  flash[:error] = e.message
-		  redirect_to date_path(date)
-	end
+ rescue Stripe::CardError, Stripe::InvalidRequestError => e
+  flash[:error] = e.message
+  redirect_to date_path(date)
+end
 
-	def notify(date)
-		client = Twilio::REST::Client.new ENV['twilio_account_sid'], ENV['twilio_auth_token']
+def notify(date)
+  client = Twilio::REST::Client.new ENV['twilio_account_sid'], ENV['twilio_auth_token']
 
-		message_to_initiator = client.messages.create(
-			:from => '+14242653879',
-			:to => "#{date.initiator.phone.gsub('-','')}",
-			:body => "Your parent has set you up on a date with #{date.recipient.first_name}",
-			)
+  message_to_initiator = client.messages.create(
+   :from => '+14242653879',
+   :to => "#{date.initiator.phone.gsub('-','')}",
+   :body => "Your parent has set you up on a date with #{date.recipient.first_name}",
+   )
 
-		message_to_recipient = client.messages.create(
-			:from => '+14242653879',
-			:to => "#{date.recipient.phone.gsub('-','')}",
-			:body => "Your parent has set you up on a date with #{date.initiator.first_name}",
-			)
-	end
+  message_to_recipient = client.messages.create(
+   :from => '+14242653879',
+   :to => "#{date.recipient.phone.gsub('-','')}",
+   :body => "Your parent has set you up on a date with #{date.initiator.first_name}",
+   )
+end
 
 end
